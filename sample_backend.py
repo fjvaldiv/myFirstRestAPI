846ef2bf5878f0e78e2c409e769a61497df695a8
Fernando Valdivia <fjvaldiv@calpoly.edu>
2021-01-13 11:35:39 -0800

    added id

--- sample_backend.py
+++ sample_backend.py
@@ -7,34 +7,34 @@ def hello_world():
 	return 'Hello, world!'
 
 users = { 
-   'users_list' :
-   [
-      { 
-         'id' : 'xyz789',
-         'name' : 'Charlie',
-         'job': 'Janitor',
-      },
-      {
-         'id' : 'abc123', 
-         'name': 'Mac',
-         'job': 'Bouncer',
-      },
-      {
-         'id' : 'ppp222', 
-         'name': 'Mac',
-         'job': 'Professor',
-      }, 
-      {
-         'id' : 'yat999', 
-         'name': 'Dee',
-         'job': 'Aspring actress',
-      },
-      {
-         'id' : 'zap555', 
-         'name': 'Dennis',
-         'job': 'Bartender',
-      }
-   ]
+    'users_list' :
+    [
+        { 
+            'id' : 'xyz789',
+            'name' : 'Charlie',
+            'job': 'Janitor',
+        },
+        {
+            'id' : 'abc123', 
+            'name': 'Mac',
+            'job': 'Bouncer',
+        },
+        {
+            'id' : 'ppp222', 
+            'name': 'Mac',
+            'job': 'Professor',
+        }, 
+        {
+            'id' : 'yat999', 
+            'name': 'Dee',
+            'job': 'Aspring actress',
+        },
+        {
+            'id' : 'zap555', 
+            'name': 'Dennis',
+            'job': 'Bartender',
+        }
+    ]
 }
 
 @app.route('/users')
@@ -46,4 +46,13 @@ def get_users():
          if user['name'] == search_username:
             subdict['users_list'].append(user)
       return subdict
+   return users
+
+@app.route('/users/<id>')
+def get_user(id):
+   if id :
+      for user in users['users_list']:
+        if user['id'] == id:
+           return user
+      return ({})
    return users
